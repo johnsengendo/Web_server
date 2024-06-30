e1b195e9c6061dfd53099085adf6d5b508de3877
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-30 23:27:47 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
+# The 'mn -c' command clears the network.
 sudo mn -c
+# Here we stops all running Docker containers.
 docker stop $(docker ps -aq)
+# This command removes all stopped Docker containers.
 docker container prune -f
