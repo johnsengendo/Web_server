f5129787b2943c42c9a42813297c4c82c7724c08
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2024-06-30 23:33:17 +0200

    Update build_docker_images.sh

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Build docker image for the video streaming server"
-docker build -t video_streaming_server --file ./Server/Dockerfile.server ./Server
+echo "Building docker image for the web server"
+docker build -t web_server --file ./Server/Dockerfile.server ./Server
 
-echo "Build docker image for the video streaming client"
-docker build -t video_streaming_client --file ./Client/Dockerfile.client ./Client
+echo "Building docker image for the web client"
+docker build -t web_client --file ./Client/Dockerfile.client ./Client
